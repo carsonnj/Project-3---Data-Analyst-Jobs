11c4f407cea3b4eabe8f5bd26b815aed55652874
thundich <tamaradurgan@gmail.com>
2024-01-17 00:06:08 -0500

    Adding OpenLayer JavaScript App

---
+++ FrontEnd/open-layers-app/main.js
@@ -0,0 +1,18 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj.js';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 3
+  })
+});
