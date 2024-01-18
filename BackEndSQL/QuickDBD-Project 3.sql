513a6b8c099489135334355963b0875d2bf9104f
carsonnj <144236409+carsonnj@users.noreply.github.com>
2024-01-17 21:26:30 -0500

    SQL Updated

--- BackEndSQL/QuickDBD-Project 3.sql
+++ BackEndSQL/QuickDBD-Project 3.sql
@@ -1,22 +1,15 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/DcKosR
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-drop TABLE jobs
+﻿drop TABLE jobs
 drop TABLE Company
 drop TABLE CompanyRating
 drop TABLE Salaries
 drop TABLE IDTable
 
-
-
--- Table 1 Job Titles
 CREATE TABLE jobs (
     JobID INT   NOT NULL,
     Job_Title VARCHAR(255)   NOT NULL,
     Salary_estimate_rage VARCHAR(255)   NOT NULL,
-    Min_Salary_Estimate INT(10) NOT NULL,
-    Max_Salary_Estimate INT(10) NOT NULL,
+    Min_Salary_Estimate INT NOT NULL,
+    Max_Salary_Estimate INT NOT NULL,
     Location VARCHAR(255) NOT NULL
 
 );
@@ -37,9 +30,9 @@ CREATE TABLE Company (
 
 -- Table 3: Company Rating
 CREATE TABLE CompanyRating (
-    CompanyID INT   NOT NULL,
+    CompanyID INT  NOT NULL,
     Company_Name VARCHAR(255)   NOT NULL,
-    Rating Decimal(2,2)   NOT NULL
+    Rating FLOAT  NOT NULL
 );
 
 -- Table 4: Salaries
@@ -51,18 +44,17 @@ CREATE TABLE Salaries (
     YearsOfExperience INT   NOT NULL,
     Salary INT   NOT NULL
 );
-
 -- Table 5 Job ID
 CREATE TABLE IDTable (
     JobTitleID Int   NOT NULL,
     CompanyID Int   NOT NULL
 );
 
---ALTER TABLE Company ADD CONSTRAINT fk_Company_CompanyID FOREIGN KEY(CompanyID)
---REFERENCES CompanyRating (CompanyID);
+ALTER TABLE Company ADD CONSTRAINT fk_Company_CompanyID FOREIGN KEY(CompanyID)
+REFERENCES CompanyRating (CompanyID);
 
---ALTER TABLE IDTable ADD CONSTRAINT fk_IDTable_JobTitleID FOREIGN KEY(JobTitleID)
---REFERENCES jobs (JobTitleID);
+ALTER TABLE IDTable ADD CONSTRAINT fk_IDTable_JobTitleID FOREIGN KEY(JobTitleID)
+REFERENCES jobs (JobTitleID);
 
---ALTER TABLE IDTable ADD CONSTRAINT fk_IDTable_CompanyID FOREIGN KEY(CompanyID)
---REFERENCES Company (CompanyID);
+ALTER TABLE IDTable ADD CONSTRAINT fk_IDTable_CompanyID FOREIGN KEY(CompanyID)
+REFERENCES Company (CompanyID);
