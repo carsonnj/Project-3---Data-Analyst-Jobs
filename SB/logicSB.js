8d7d41516176cbce4d61a96157ae82fa753e05a4
Tika26 <shivantikas@yahoo.com>
2024-01-21 17:35:28 -0500

    add SB

---
+++ SB/logicSB.js
@@ -0,0 +1,59 @@
+
+
+d3.csv('./sectorSB.csv').then(data=>{
+
+    let sectors = [...new Set(data.map(obj=> obj.Sector))];
+    let values = [];
+
+    
+    sectors.forEach(sec => {
+        values.push(data.filter(obj=>obj.Sector == sec).length);
+    });
+    console.log(values);
+
+    var data = [
+        {
+          x: sectors,
+          y: values,
+          type: 'bar'
+        }
+      ];
+
+      var layout = {
+        title: {
+          text: 'Sector Vs Job Availability',
+          font: {
+          famity: 'Arial, Italic',
+          size: 30,
+          color: 'brown'
+        },
+        xaxis:{
+          title: {
+            text: 'Sectors',
+            font: {
+              size: 26,
+              color: 'darkgreen',
+            }
+
+          },
+          tickformate: {
+          
+          }
+        },
+        yaxis:{
+          title: {
+            text: 'Jobs',
+            font: {
+              size: 26,
+              color: 'green',
+            }
+
+          },
+        },
+
+      
+      }
+    };
+      Plotly.newPlot('jobsCount', data, layout);
+
+});
