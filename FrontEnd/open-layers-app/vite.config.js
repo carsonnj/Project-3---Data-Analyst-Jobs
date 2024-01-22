13a39149e3cce0b525a4e907b9a99ced1fd15898
thundich <tamaradurgan@gmail.com>
2024-01-21 21:32:31 -0500

    Update vite build configs

--- FrontEnd/open-layers-app/vite.config.js
+++ FrontEnd/open-layers-app/vite.config.js
@@ -1,12 +1,17 @@
-import { resolve } from "path";
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-export default {
+export default defineConfig({
   build: {
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        map: resolve(__dirname, 'data/map.html'),
+        salary: resolve(__dirname, 'data/salary.html'),
+        sector: resolve(__dirname, 'data/sector.html')
+      },
+    },
   },
-  rollupOptions: {
-    input: {
-        main: resolve(__dirname, 'dist/index.html'),
-    }
-  }
-}
+})
