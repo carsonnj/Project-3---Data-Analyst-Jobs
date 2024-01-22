99695b817eb634b3a21bc5ce53367fc016aa5c5e
thundich <tamaradurgan@gmail.com>
2024-01-21 19:00:39 -0500

    add rollupOptions to vite.config.js

--- FrontEnd/open-layers-app/vite.config.js
+++ FrontEnd/open-layers-app/vite.config.js
@@ -1,5 +1,12 @@
+import { resolve } from "path";
+
 export default {
   build: {
     sourcemap: true,
+  },
+  rollupOptions: {
+    input: {
+        main: resolve(__dirname, 'index.html'),
+    }
   }
 }
